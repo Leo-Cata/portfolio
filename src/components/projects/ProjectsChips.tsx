7563c767882c845bcb2e430b9993e62aea92ade9
Leo <leonel.m.cata03@gmail.com>
2023-12-13 18:42:40 -0300

    mend

--- src/components/projects/ProjectsChips.tsx
+++ src/components/projects/ProjectsChips.tsx
@@ -2,11 +2,7 @@ import { ProjectChips } from "../../types/types";
 
 const ProjectsChips = ({ name, bg }: ProjectChips) => {
   return (
-    <div
-      className={`m-1 rounded-md px-3 font-semibold ${bg} ${
-        bg === "bg-javascript" ? "text-black" : "text-black"
-      }`}
-    >
+    <div className={`m-1 rounded-md px-3 font-semibold ${bg} text-black}`}>
       {name}
     </div>
   );
