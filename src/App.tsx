a899c3dde899b1096e8e3086995600906e2fa5be
Leo <leonel.m.cata03@gmail.com>
2024-01-09 15:59:16 -0300

    wip lang change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import MainPage from "./containers/MainPage";
 
 function App() {
   return (
     <div className="my-10">
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<Navigate to="/en" replace />} />
+        <Route path="/:lang" element={<MainPage />} />
       </Routes>
     </div>
   );
--- src/containers/MainPage.tsx
+++ src/containers/MainPage.tsx
@@ -1,9 +1,24 @@
+import { useNavigate, useParams } from "react-router-dom";
 import Hero from "../components/Hero";
 import Projects from "../components/projects/Projects";
 
 const MainPage = () => {
+  const lang = useParams();
+  console.log("🚀 ~ MainPage ~ lang:", lang);
+  const nav = useNavigate();
+
+  const handleNav = () => {
+    if (lang.lang === "en") {
+      nav("/es");
+      console.log("es");
+    } else {
+      nav("/en");
+      console.log("en");
+    }
+  };
   return (
     <>
+      <button onClick={handleNav}>Change Language</button>
       <Hero />
       <Projects />
     </>
